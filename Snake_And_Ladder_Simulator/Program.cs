bf7d01993d23c5412f22934f1fb758a94b5cee05
Ankit Singh Rathore <asinghrathore0@gmail.com>
2021-07-25 16:53:49 +0530

    [Ankit] Add : Snake And Ladder Project

---
+++ Snake_And_Ladder_Simulator/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Snake_And_Ladder_Simulator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to The Snake And Ladder Simulator");
+        }
+    }
+}
