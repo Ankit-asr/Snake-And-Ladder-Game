bd7a2978fba7490f5872221e08da228d85eaa18f
Ankit Singh Rathore <asinghrathore0@gmail.com>
2021-07-25 20:54:53 +0530

    [Ankit] Add : Player checks for an option among three

--- Snake_And_Ladder_Simulator/SnakeAndLadder.cs
+++ Snake_And_Ladder_Simulator/SnakeAndLadder.cs
@@ -4,13 +4,39 @@ namespace Snake_And_Ladder_Simulator
 {
     class SnakeAndLadder
 	{
-		int startPosition = 0;
-		public void RollsDice()
+		const int START_POSITION = 0;
+	    const int NO_PLAY = 0;
+		const int LADDER = 1;
+		const int SNAKE = 2;
+
+		public void GetOption()
 		{
-			Console.WriteLine("Player starting position : " + startPosition);
+			int currentPlayerPosition = START_POSITION;
+			Console.WriteLine("Your starting position : " + START_POSITION);
+			Console.WriteLine("Press Enter to Roll Die");
+			Console.ReadKey();
 			Random random = new Random();
-			int diceRoll = random.Next(1, 7);
-			Console.WriteLine("Player Got : " + diceRoll);
+			int rollResult = random.Next(1, 7);
+			Console.WriteLine(" You Got : " + rollResult);
+            Console.WriteLine(" Press Enter to check for an option ");
+            Console.ReadKey();
+			int option = random.Next(0, 3);
+			switch (option)
+			{
+				case (NO_PLAY):
+					Console.WriteLine(" You got No Play");
+					break;
+				case (LADDER):
+					Console.WriteLine(" You got Ladder");
+					currentPlayerPosition += rollResult;
+					Console.WriteLine(" Your position after dice :" + currentPlayerPosition);
+					break;
+				case (SNAKE):
+					Console.WriteLine(" You got Snake");
+					currentPlayerPosition -= rollResult;
+					Console.WriteLine(" Your position after dice :" + currentPlayerPosition);
+					break;
+			}
 		}
 	}
 }
