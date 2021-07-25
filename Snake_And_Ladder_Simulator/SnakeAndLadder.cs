82f9308167950d2053ea3de521fc7eda175d9cc2
Ankit Singh Rathore <asinghrathore0@gmail.com>
2021-07-26 00:04:16 +0530

    [Ankit] Add: Player keeps playing till Winning Position

--- Snake_And_Ladder_Simulator/SnakeAndLadder.cs
+++ Snake_And_Ladder_Simulator/SnakeAndLadder.cs
@@ -8,35 +8,42 @@ namespace Snake_And_Ladder_Simulator
 		const int LADDER = 1;
 		const int SNAKE = 2;
 		const int WINNING_POSITION = 100;
-		public void CheckOption()
+		public void PlayTillWin()
 		{
 			int currentPlayerPosition = 0;
 			Console.WriteLine("Your starting position : " + currentPlayerPosition);
-			Console.WriteLine("Press Enter to Roll Die");
-			Console.ReadKey();
-			Random random = new Random();
-			int rollResult = random.Next(1, 7);
-			Console.WriteLine(" You Got : " + rollResult);
-			Console.WriteLine(" Press Enter to check for an option ");
-			Console.ReadKey();
-			int option = random.Next(0, 3);
-			switch (option)
+			while (currentPlayerPosition < WINNING_POSITION)
 			{
-				case (NO_PLAY):
-					Console.WriteLine(" You got No Play");
-					Console.WriteLine("Your position after die roll : " + currentPlayerPosition);
-					break;
-				case (LADDER):
-					Console.WriteLine(" You got Ladder");
-					currentPlayerPosition += rollResult;
-					Console.WriteLine(" Your position after die roll :" + currentPlayerPosition);
-					break;
-				case (SNAKE):
-					Console.WriteLine(" You got Snake");
-					currentPlayerPosition -= rollResult;
-					Console.WriteLine(" Your position after die roll :" + currentPlayerPosition);
-					break;
+				Console.WriteLine("Press Enter to Roll Die");
+				Console.ReadKey();
+				Random random = new Random();
+				int rollResult = random.Next(1, 7);
+				Console.WriteLine(" You Got : " + rollResult);
+				Console.WriteLine(" Press Enter to check for an option ");
+				Console.ReadKey();
+				int option = random.Next(0, 3);
+				switch (option)
+				{
+					case (NO_PLAY):
+						Console.WriteLine(" You got No Play");
+						Console.WriteLine("Your position after die roll : " + currentPlayerPosition);
+						break;
+					case (LADDER):
+						Console.WriteLine(" You got Ladder");
+						currentPlayerPosition += rollResult;
+						Console.WriteLine(" Your position after die roll :" + currentPlayerPosition);
+						break;
+					case (SNAKE):
+						Console.WriteLine(" You got Snake");
+						if (currentPlayerPosition >= rollResult)
+							currentPlayerPosition -= rollResult;
+						else
+							currentPlayerPosition = 0;
+							Console.WriteLine(" Your position after die roll :" + currentPlayerPosition);
+						break;
+				}
 			}
 		}
 	}
 }
+
